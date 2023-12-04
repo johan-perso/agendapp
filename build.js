dc9790c2cde07e6a5115633320b47d8e74d3aa6d
Johan le stickman <Johan_Stickman@proton.me>
2023-12-04 21:48:00 +0100

    Fix impossibilité de build en dehors de macOS

--- build.js
+++ build.js
@@ -18,37 +18,39 @@ async function main(){
 	var commands = [
 		"npm run build_tailwindcss",
 		"npm run build-exe-x64",
-		"npm run build-app-x64",
-		"npm run build-app-arm64"
-	]
+		process.platform == "darwin" ? "npm run build-app-x64" : null,
+		process.platform == "darwin" ? "npm run build-app-arm64" : null
+	].filter(e => e != null)
 	for (var i = 0; i < commands.length; i++) {
 		console.log(`@@@@ ${commands[i]}`)
 		childProcess.execSync(commands[i])
 	}
 
 	// On build un fichier dmg pour macOS
-	var macOsSupportedArchs = [
-		"arm64", "x64"
-	]
-	for(var i = 0; i < macOsSupportedArchs.length; i++) {
-		// On fait la commande
-		var command = `npm run create-dmg-cli ${path.join(__dirname, "release-builds", `Agendapp-darwin-${macOsSupportedArchs[i]}`, "Agendapp.app")} release-builds`
-		console.log(`@@@@ ${command}`)
-		try { childProcess.execSync(command) } catch(e) {}
+	if(process.platform == "darwin"){
+		var macOsSupportedArchs = [
+			"arm64", "x64"
+		]
+		for(var i = 0; i < macOsSupportedArchs.length; i++) {
+			// On fait la commande
+			var command = `npx create-dmg ${path.join(__dirname, "release-builds", `Agendapp-darwin-${macOsSupportedArchs[i]}`, "Agendapp.app")} release-builds`
+			console.log(`@@@@ ${command}`)
+			try { childProcess.execSync(command) } catch(e) {}
 
-		// On renomme le fichier dmg généré
-		fs.renameSync(
-			path.join(__dirname, "release-builds", `Agendapp ${version}.dmg`),
-			path.join(__dirname, "release-builds", `agendapp-${version}-macos-${macOsSupportedArchs[i]}.dmg`)
-		)
-	}
+			// On renomme le fichier dmg généré
+			fs.renameSync(
+				path.join(__dirname, "release-builds", `Agendapp ${version}.dmg`),
+				path.join(__dirname, "release-builds", `agendapp-${version}-macos-${macOsSupportedArchs[i]}.dmg`)
+			)
+		}
+	} else console.log("@@@@ Pas de build pour macOS car on est pas sur macOS")
 
 	// On compresse les dossiers (pour pouvoir les mettre sur GitHub)
 	var releaseBuildsFiles = [
 		{ platform: "win32", arch: "x64", input: path.join(__dirname, "release-builds", "Agendapp-win32-x64"), output: path.join(__dirname, "release-builds", `agendapp-${version}-win32-x64.zip`) },
-		{ platform: "darwin", arch: "x64", input: path.join(__dirname, "release-builds", "Agendapp-darwin-x64"), output: path.join(__dirname, "release-builds", `agendapp-${version}-macos-x64.zip`) },
-		{ platform: "darwin", arch: "arm64", input: path.join(__dirname, "release-builds", "Agendapp-darwin-arm64"), output: path.join(__dirname, "release-builds", `agendapp-${version}-macos-arm64.zip`) }
-	]
+		process.platform == "darwin" ? { platform: "darwin", arch: "x64", input: path.join(__dirname, "release-builds", "Agendapp-darwin-x64"), output: path.join(__dirname, "release-builds", `agendapp-${version}-macos-x64.zip`) } : null,
+		process.platform == "darwin" ? { platform: "darwin", arch: "arm64", input: path.join(__dirname, "release-builds", "Agendapp-darwin-arm64"), output: path.join(__dirname, "release-builds", `agendapp-${version}-macos-arm64.zip`) } : null
+	].filter(e => e != null)
 	for(var i = 0; i < releaseBuildsFiles.length; i++) {
 		console.log(`@@@@ Création du zip pour ${releaseBuildsFiles[i].platform} ${releaseBuildsFiles[i].arch}`)
 		var archive = archiver("zip", { zlib: { level: 9 } })
