21cf85893a4ae6c713384b772e07357ab89700ca
Johan le stickman <johan@johanstick.fr>
2024-04-28 19:01:02 +0200

    Jfais des tests

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ const fs = require("fs")
 const path = require("path")
 const archiver = require("archiver")
 const version = require("./package.json").version
+const appx = require("./package.json").build.appx
+const convertToWindowsStore = require("electron-windows-store")
 
 async function main(){
 	// Si le dossier release-builds n'existe pas, on le crée, sinon on le vide
@@ -60,11 +62,26 @@ async function main(){
 		console.log(`@@@@ Zip créé pour ${releaseBuildsFiles[i].platform} ${releaseBuildsFiles[i].arch}`)
 	}
 
+	// On va build en .appx (Windows Store)
+	if(process.platform == "win32"){
+		console.log("@@@@ Création de l'application pour le Windows Store")
+		var packageVersion = version.split(".").length == 3 ? `${version}.0` : version
+		await convertToWindowsStore({
+			packageVersion: packageVersion,
+			packageName: "agendapp",
+			packageDisplayName: appx.displayName,
+			packageExecutable: path.join(__dirname, "release-builds", "Agendapp-win32-x64", "Agendapp.exe"),
+			deploy: false,
+			publisher: appx.publisher,
+		})
+		console.log("@@@@ Application pour le Windows Store créée")
+	} else console.log("@@@@ Pas de build pour le Windows Store car on est pas sur Windows")
+
 	// Nettoyage
 	console.log("@@@@ Nettoyage")
 	var files = fs.readdirSync(path.join(__dirname, "release-builds"))
 	files.forEach(file => {
-		if(!file.endsWith(".dmg") && !file.endsWith(".zip")) fs.rmSync(path.join(__dirname, "release-builds", file), { recursive: true })
+		if(!file.endsWith(".dmg") && !file.endsWith(".zip") && !file.endsWith(".appx")) fs.rmSync(path.join(__dirname, "release-builds", file), { recursive: true })
 	})
 	console.log("@@@@ Nettoyage terminé")
 }
